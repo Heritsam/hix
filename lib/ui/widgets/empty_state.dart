bfbe4894382adabb42899734be95db23fa76aaae
heritsam <ariqhm@outlook.com>
2020-05-19 22:22:55 +0700

    add empty state

---
+++ lib/ui/widgets/empty_state.dart
@@ -0,0 +1,35 @@
+part of 'widget.dart';
+
+class EmptyState extends StatelessWidget {
+  final String label;
+
+  const EmptyState({Key key, @required this.label}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.all(64.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image(
+              image: AssetImage('assets/empty_state.png'),
+              fit: BoxFit.contain,
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              label,
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
