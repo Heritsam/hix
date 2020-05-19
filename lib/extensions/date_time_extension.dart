6ad678226b8c16343d7173ec203790f7bf846a74
heritsam <ariqhm@outlook.com>
2020-05-19 22:23:17 +0700

    change . to ,

--- lib/extensions/date_time_extension.dart
+++ lib/extensions/date_time_extension.dart
@@ -1,7 +1,7 @@
 part of 'extension.dart';
 
 extension DateTimeExtension on DateTime {
-  String get dateAndTime => "${this.shortDayName} ${this.day}. ${this.hour}:00";
+  String get dateAndTime => "${this.shortDayName} ${this.day}, ${this.hour}:00";
 
   String get shortDayName {
     switch (this.weekday) {
